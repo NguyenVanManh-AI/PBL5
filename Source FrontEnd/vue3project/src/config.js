fe473e91dc5b6a4c5b03314375d27a88f28505bd
github_username <nguyenvanmanh2001it1@gmail.com>
2023-03-26 14:24:51 +0700

    add99

--- Source FrontEnd/vue3project/src/config.js
+++ Source FrontEnd/vue3project/src/config.js
@@ -1,11 +1,6 @@
 export default{
 	// API_URL :'http://localhost:3000' , //API_URL 
-<<<<<<< HEAD
 	API_URL : 'http://127.0.0.1:8000'
 	// API_URL : 'https://pbl5.up.railway.app'
-=======
-	// API_URL : 'http://127.0.0.1:8000'
-	API_URL : 'https://pbl5.up.railway.app' 
->>>>>>> bfee4d6d7fa4502287453cb76b916778b397c33f
 	// API_URL : 'http://nguyenvanmanh.lovestoblog.com'
 }
