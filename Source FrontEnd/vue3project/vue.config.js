9b3c6fb81f4f28c52b4f7b5b3cb316cc8c903a53
github_username <nguyenvanmanh2001it1@gmail.com>
2023-03-26 13:55:37 +0700

    add12

--- Source FrontEnd/vue3project/vue.config.js
+++ Source FrontEnd/vue3project/vue.config.js
@@ -1,14 +1,4 @@
-const { defineConfig } = require("@vue/cli-service");
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false,
-  configureWebpack: {
-    plugins: [new NodePolyfillPlugin()],
-    optimization: {
-      splitChunks: {
-        chunks: "all",
-      },
-    },
-  },
-}); 
+  transpileDependencies: true
+})
