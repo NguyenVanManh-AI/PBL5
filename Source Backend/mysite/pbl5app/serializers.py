c1ff427216a285c05d4b15a8dbf69079a87b0dca
Cuong Nguyen Cong <cuongtk2002@example.com>
2023-04-23 15:13:59 +0700

    change_password

--- Source Backend/mysite/pbl5app/serializers.py
+++ Source Backend/mysite/pbl5app/serializers.py
@@ -6,3 +6,10 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'email', 'role', 'password', 'fullname', 'url_video', 'phone', 'create_at', 'update_at']
 
+class UserPasswordUpdateSerializer(serializers.ModelSerializer):
+    old_password = serializers.CharField(write_only=True, required=True)
+    new_password = serializers.CharField(write_only=True, required=True)
+    
+    class Meta:
+        model = User
+        fields = ('old_password', 'new_password')
