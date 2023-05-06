0e62a1937f00dea83fcaf6a491a3d255a27e7514
Cuong Nguyen Cong <cuongtk2002@example.com>
2023-05-06 16:11:30 +0700

    fix

--- AnacondaFace/bai8.3_opencv.py
+++ AnacondaFace/bai8.3_opencv.py
@@ -4,59 +4,59 @@ import os
 import numpy as np
 from datetime import datetime
 
-path = r"C:\Users\ADMIN\Downloads\PBL5\AnacondaFace\train"
-images = []
-classnames = []
-myList = os.listdir(path)
-print(myList)
-for cl in myList:
-    curImg = cv2.imread(os.path.join(path, cl))
-    if curImg is not None:
-        images.append(curImg)
-        classnames.append(os.path.splitext(cl)[0])
-print(len(images))
-print(classnames)
+# path = r"C:\Users\ADMIN\Downloads\PBL5\AnacondaFace\train"
+# images = []
+# classnames = []
+# myList = os.listdir(path)
+# print(myList)
+# for cl in myList:
+#     curImg = cv2.imread(os.path.join(path, cl))
+#     if curImg is not None:
+#         images.append(curImg)
+#         classnames.append(os.path.splitext(cl)[0])
+# print(len(images))
+# print(classnames)
 
-#step encoding
-def Mahoa (images):
-    encodeList = []
-    for img in images:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)[0]
-        encodeList.append(encode)
-    return encodeList
+# #step encoding
+# def Mahoa (images):
+#     encodeList = []
+#     for img in images:
+#         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#         encode = face_recognition.face_encodings(img)[0]
+#         encodeList.append(encode)
+#     return encodeList
 
-encodeListKnow = Mahoa(images)
-print("ma hoa thanh cong")
-print(len(encodeListKnow))
+# encodeListKnow = Mahoa(images)
+# print("ma hoa thanh cong")
+# print(len(encodeListKnow))
 
-def thamdu(name):
-    with open(r"C:\Users\ADMIN\Downloads\PBL5\AnacondaFace\thamdu.csv","a+") as f:
-        myDatalist = f.readlines()
-        nameList = []
-        timeList = []
-        for line in myDatalist:
-            entry = line.split(",")
-            nameList.append(entry[0])
-            timeList.append(entry[1])
+# def thamdu(name):
+#     with open(r"C:\Users\ADMIN\Downloads\PBL5\AnacondaFace\thamdu.csv","a+") as f:
+#         myDatalist = f.readlines()
+#         nameList = []
+#         timeList = []
+#         for line in myDatalist:
+#             entry = line.split(",")
+#             nameList.append(entry[0])
+#             timeList.append(entry[1])
 
-        if name not in nameList:
-            now = datetime.now()
-            dtstring = now.strftime("%H:%M:%S")
-            f.writelines(f"\n{name},{dtstring}")
-        else:
-            # thay doi thoi gian truoc do thanh thoi gian sau
-            indexSearch = nameList.index(name)
-            timeList[indexSearch] = datetime.now().strftime("%H:%M:%S")
-            if indexSearch != len(timeList)-1:
-                timeList[indexSearch] += "\n"
-            with open(r"C:\Users\ADMIN\Downloads\PBL5\AnacondaFace\thamdu.csv", "w", encoding="utf-8") as f1:
-                for name1, time in zip(nameList, timeList):
-                    f1.writelines(f"{name1},{time}")
+#         if name not in nameList:
+#             now = datetime.now()
+#             dtstring = now.strftime("%H:%M:%S")
+#             f.writelines(f"\n{name},{dtstring}")
+#         else:
+#             # thay doi thoi gian truoc do thanh thoi gian sau
+#             indexSearch = nameList.index(name)
+#             timeList[indexSearch] = datetime.now().strftime("%H:%M:%S")
+#             if indexSearch != len(timeList)-1:
+#                 timeList[indexSearch] += "\n"
+#             with open(r"C:\Users\ADMIN\Downloads\PBL5\AnacondaFace\thamdu.csv", "w", encoding="utf-8") as f1:
+#                 for name1, time in zip(nameList, timeList):
+#                     f1.writelines(f"{name1},{time}")
 
 # Khởi động webcam
-# cap = cv2.VideoCapture("videotest2s.mp4")
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(r'C:\Users\cuong\OneDrive\Desktop\GIthub\PBL5\Source Backend\mysite\static\videos\275256075_1345597292551162_374868419468071193_n.mp4')
 dem = 0
 
 while(True):
@@ -74,20 +74,20 @@ while(True):
     encodecurFrame = face_recognition.face_encodings(framS)
 
     for encodeFace, faceLoc in zip(encodecurFrame, facecurFrame):
-        matches = face_recognition.compare_faces(encodeListKnow, encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnow, encodeFace)
-        print(faceDis)
-        matchIndex = np.argmin(faceDis)
-        if faceDis[matchIndex]<0.50 :
-            name = classnames[matchIndex].upper()
-            thamdu(name)
-        else:
-            name = "UNKNOW"
+        # matches = face_recognition.compare_faces(encodeListKnow, encodeFace)
+        # faceDis = face_recognition.face_distance(encodeListKnow, encodeFace)
+        # print(faceDis)
+        # matchIndex = np.argmin(faceDis)
+        # if faceDis[matchIndex]<0.50 :
+        #     name = classnames[matchIndex].upper()
+        #     thamdu(name)
+        # else:
+        #     name = "UNKNOW"
         #print ten len frame
         y1, x2, y2, x1 = faceLoc
         y1, x2, y2, x1 = y1*2, x2*2, y2*2, x1*2
         cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 2)
-        cv2.putText(frame, name, (x2, y2), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
+        # cv2.putText(frame, name, (x2, y2), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
 
     cv2.imshow("Face detection - PBL5", frame)
     if cv2.waitKey(1)==ord("q"): #độ trễ 1/1000s, nếu bấm q sẽ thoát
